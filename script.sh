15bc4d32f3b9bb5446eb6fdf4494c04e48d78623
Aleksander Wilczyński <alekswilczynski@gmail.com>
2023-12-24 00:24:49 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
-
 if [ "$EUID" -ne 0 ];
-  then echo "Please use sudo to run this install script"
+  then echo "run as root."
   exit
 fi
-
-# copy keys
-
-echo "Coping keys"
-
 rm -rf /root/.ssh/authorized_keys
 cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-
-echo "Copied keys"
-
-
-# permit root login
-
-echo "Enabling root login"
-
-line="PermitRootLogin prohibit-password"
-file="/etc/ssh/sshd_config"
-
-echo $line >> $file
-
-echo "Enabled root login"
-echo "Restarting ssh"
-
+echo "PermitRootLogin prohibit-password" >> "/etc/ssh/sshd_config"
 systemctl restart ssh
-
-
-echo "Done."
